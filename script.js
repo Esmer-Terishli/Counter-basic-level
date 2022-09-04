3f0a82f1a3e0ea484ff6411f6b8748c70723f9fb
Esmer-Terishli <esmerterishli@mail.ru>
2022-09-05 01:41:01 +0400

    First trial

---
+++ script.js
@@ -0,0 +1,73 @@
+const counter = document.getElementById("counter");
+const incr= document.querySelector(".increase");
+const decr = document.querySelector(".decrease");
+
+let count = 0;
+
+incr.addEventListener("click", ()=> {
+    count++;
+    counter.innerHTML = count;
+});
+
+decr.addEventListener("click", ()=>{
+    count--;
+    counter.innerHTML = count;
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const counter = document.querySelector(".counter");
+// const incr= document.querySelector(".increase");
+// const decr = document.querySelector(".decrease");
+
+// let count = 0;
+// incr.addEventListener("click", ()=> {
+//     count++;
+//     counter.innerHTML = count;
+// });
+
+// decr.addEventListener("click", ()=> {
+//     count--;
+//     counter.innerHTML = count;
+// });
